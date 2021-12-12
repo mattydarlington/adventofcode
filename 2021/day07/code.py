3323be39b82c5d3181c22a1aa36e8aa8ca52d50f
Matthew Darlington <matthewd375@hotmail.co.uk>
2021-12-12 11:57:46 +0000

    Day 7 2021 Done

---
+++ 2021/day07/code.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#### Read Data from txt file
+#### and put in list named Data
+
+data = np.loadtxt('data.txt', delimiter = ',')
+
+print('Answer for Day 7 Part 1 is:', sum(abs(data - np.median(data))))
+
+# Need a smarter way for day 2
+# Solve with recursion and cache the outputs
+
+def triangle(n):
+    return n/2*(n+1)
+
+def cost(crab, pos):
+    return triangle(abs(crab-pos))
+
+total_costs = np.zeros(int(max(data)))
+for i in range(int(max(data))):
+    for crab in data:
+        total_costs[i] += cost(crab, i)
+    
+
+print('Answer for Day 7 Part 2 is:', min(total_costs))   
